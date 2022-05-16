c66d42f1ff7c37cd50fdd9fa61e9f8f7491d1d85
Cheespeasa1234 <45641361+Cheespeasa1234@users.noreply.github.com>
2022-05-16 17:34:31 -0400

    web spider

--- src/Alien.java
+++ src/Alien.java
@@ -3,20 +3,29 @@ import java.awt.Rectangle;
 
 public class Alien {
 	
-	int x,y,w,h,dx,speed=10,prevX;
-	int imageFrame = 0;
+	int x,y,w,h,dx,speed=10,prevX,imageFrame = 0,row;
 	Image[] anim;
 	
-	public Alien(int x, int y, int w, int h, Image[] anim) {
+	public Alien(int x, int y, int w, int h, Image[] anim, int row) {
 		this.x=x;
 		this.y=y;
 		this.w=w;
 		this.h=h;
 		this.dx=speed;
 		this.anim = anim;
+		this.row = row;
 	}
 	
 	public Rectangle getHitbox() {
 		return new Rectangle(x,y,w,h);
+	}
+	
+	public int getRow() {
+		return this.row;
+	}
+	
+	@Override
+	public String toString() {
+		return "Alien"+this.hashCode()+"[x="+this.x+",y="+this.y+",frame="+this.imageFrame+"]";
 	}
 }
--- src/Game.java
+++ src/Game.java
@@ -25,7 +25,10 @@ import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 import javax.swing.Timer;
 
-import java.util.*;
+import java.util.ArrayList;
+
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
 
 //Honors Computer Science - Mr. Uhl
 //Program description: A template class for creating graphical applications
@@ -36,17 +39,18 @@ public class Game extends JPanel implements MouseListener, KeyListener, FocusLis
 	private static final long serialVersionUID = 1L;
 	public static final int PREF_W = 224 * 3;
 	public static final int PREF_H = 256 * 3;
-	
-	//assets
+
+	// assets
 	private AssetManager am = new AssetManager("as/");
 	private Image playerImage = am.getImage("img/ship.png");
 	private Image explosion = am.getImage("img/dead.png");
-	private Image[] alien1Frames = {am.getImage("img/Alien1-a.png"),am.getImage("img/Alien1-b.png"),explosion};
-	private Image[] alien2Frames = {am.getImage("img/Alien2-a.png"),am.getImage("img/Alien2-b.png"),explosion};
-	private Image[] alien3Frames = {am.getImage("img/Alien3-a.png"),am.getImage("img/Alien3-b.png"),explosion};
+	private Image[] alien1Frames = { am.getImage("img/Alien1-a.png"), am.getImage("img/Alien1-b.png"), explosion };
+	private Image[] alien2Frames = { am.getImage("img/Alien2-a.png"), am.getImage("img/Alien2-b.png"), explosion };
+	private Image[] alien3Frames = { am.getImage("img/Alien3-a.png"), am.getImage("img/Alien3-b.png"), explosion };
 
 	private ArrayList<Alien> aliens = new ArrayList<Alien>();
 	private ArrayList<Explosion> explosions = new ArrayList<Explosion>();
+	private ArrayList<Alien> highestYaliens = new ArrayList<Alien>();
 	private Laser playerLaser = null;
 	private Player player = new Player();
 
@@ -63,9 +67,10 @@ public class Game extends JPanel implements MouseListener, KeyListener, FocusLis
 	private int random(int a, int b) {
 		return (int) Math.floor(Math.random() * (b - a + 1) + a);
 	}
-	
+
 	private Timer wait500 = new Timer(500, new ActionListener() {
 		public void actionPerformed(ActionEvent event) {
+			println("Waiting 500 ms.", "UPDATE");
 			explosions.clear();
 			wait500.stop();
 		}
@@ -83,21 +88,27 @@ public class Game extends JPanel implements MouseListener, KeyListener, FocusLis
 	});
 
 	private void drawPlaying(Graphics2D g2) {
-		
-		if(playState.equals("Playing")) {
+
+		if (playState.equals("Playing")) {
+			g2.setColor(Color.RED);
 			// draw the aliens
 			for (Alien a : aliens) {
 				Image f = a.anim[a.imageFrame];
-				g2.drawImage(f, a.x, a.y, a.w + ((a.imageFrame==3)?50:0), a.h, this);
+				if (highestYaliens.indexOf(a) > -1) {
+
+					g2.fillRect(a.x, a.y, a.w + ((a.imageFrame == 3) ? 50 : 0), a.h);
+
+				}
+				g2.drawImage(f, a.x, a.y, a.w + ((a.imageFrame == 3) ? 50 : 0), a.h, this);
+
 			}
 
 			// draw the lasers
-			g2.setColor(Color.RED);
-			if(playerLaser != null)
+			if (playerLaser != null)
 				g2.fill(playerLaser.getHitbox());
-			
+
 			// draw the explosions
-			for(int i = 0; i < explosions.size(); i++) {
+			for (int i = 0; i < explosions.size(); i++) {
 				Explosion e = explosions.get(i);
 				g2.drawImage(e.i, e.x, e.y, e.i.getWidth(null), e.i.getHeight(null), null);
 			}
@@ -123,11 +134,11 @@ public class Game extends JPanel implements MouseListener, KeyListener, FocusLis
 			g2.drawString("Firing timer: " + fireCooldown + " / " + maxCooldown, 0, 30);
 
 			g2.drawString(lives + "", 10, PREF_H - 10);
-		} else if(playState.equals("Paused")) {
+		} else if (playState.equals("Paused")) {
 			String text = "Press <SPACE> to resume!";
 			FontMetrics fm = g2.getFontMetrics();
-			
-			g2.drawString(text, PREF_W/2 - fm.stringWidth(text)/2, 100);
+
+			g2.drawString(text, PREF_W / 2 - fm.stringWidth(text) / 2, 100);
 		}
 
 	}
@@ -137,10 +148,10 @@ public class Game extends JPanel implements MouseListener, KeyListener, FocusLis
 		// Player physics
 		boolean L = a || l;
 		boolean R = d || r;
-		
+
 		int s = 5;
-		//player.dx = (L == R) ? 0 : (L) ? -s : s;
-		player.dx=L==R?0:L?-s:s;
+		// player.dx = (L == R) ? 0 : (L) ? -s : s;
+		player.dx = L == R ? 0 : L ? -s : s;
 		player.x += player.dx;
 
 		// Collide with sides
@@ -150,42 +161,74 @@ public class Game extends JPanel implements MouseListener, KeyListener, FocusLis
 			player.x = PREF_W - player.w;
 
 		// Laser movement
-		if(playerLaser != null) {
+		if (playerLaser != null) {
 			playerLaser.y += playerLaser.dy;
-			
-			if(playerLaser.y < 0) {
-				explosions.add(new Explosion(explosion, playerLaser.x - explosion.getWidth(null)/2, playerLaser.y));
+
+			if (playerLaser.y < 0) {
+				explosions.add(new Explosion(explosion, playerLaser.x - explosion.getWidth(null) / 2, playerLaser.y));
 				playerLaser = null;
 				wait500.start();
 			}
 		}
-		
+
 		// Laser killing
-		for(int i = 0; i < aliens.size(); i++)
-			if(playerLaser!=null&&playerLaser.getHitbox().intersects(aliens.get(i).getHitbox())) {
+		for (int i = 0; i < aliens.size(); i++)
+			if (playerLaser != null && playerLaser.getHitbox().intersects(aliens.get(i).getHitbox())) {
 				aliens.get(i).imageFrame = 2;
 				playerLaser = null;
 				movementCount = 0;
-			}
-		
 
+				Alien highest = null;
+
+				// reset highest alien
+				for (Alien a : aliens) {
+					if (a.getRow() == aliens.get(i).getRow()) {
+						highest = a;
+						break;
+					}
+				}
+
+				for (Alien a : aliens) {
+					if (a.getRow() == aliens.get(i).getRow() && a.y > highest.y) {
+						highest = a;
+					}
+				}
+
+			}
 
 		// Alien gunfire
-		fireCooldown++;
-		if (fireCooldown >= maxCooldown) {
-			fireCooldown = 0;
-
-			maxCooldown = random(15, 30) - (aliensKilled / 3);
-
-		}
 
 		// Alien movement
+		int startingAliens = 5 * 11;
 		movementCount++;
 		if (movementCount == maxWaitTime) {
-			
+
 			// remove exploded aliens
-			for(int i = 0; i < aliens.size(); i++) {
-				if(aliens.get(i).imageFrame == 2) {
+			for (int i = 0; i < aliens.size(); i++) {
+				Alien a = aliens.get(i);
+				if (a.imageFrame == 2) {
+
+					if (highestYaliens.indexOf(a) > -1) {
+
+						println("Testing for new lowest alien. Alien shot: " + a.toString(), "UPDATE");
+
+						Alien nextHighest = a;
+
+						int highestY = 0;
+						for (int j = 0; j < aliens.size(); j++) {
+							Alien b = aliens.get(j);
+							if (b.x == a.x && b.y > highestY) {
+								
+								println("Winner found, " + a.toString() + " competing against " + highestY, "UPDATE");
+								highestY = b.y;
+								nextHighest = b;
+							}
+						}
+
+						highestYaliens.set(highestYaliens.indexOf(a), nextHighest);
+
+					}
+
 					aliens.remove(i);
 				}
 			}
@@ -196,15 +239,15 @@ public class Game extends JPanel implements MouseListener, KeyListener, FocusLis
 			for (int i = 0; i < aliens.size(); i++) {
 
 				Alien a = aliens.get(i);
-				
+
 				// Move the alien
 				a.prevX = a.x;
 				a.x += a.dx;
-				
+
 				// Change animation frame
-				if(a.imageFrame == 0)
+				if (a.imageFrame == 0)
 					a.imageFrame = 1;
-				else if(a.imageFrame == 1)
+				else if (a.imageFrame == 1)
 					a.imageFrame = 0;
 
 				// Detect alien on sides
@@ -237,21 +280,28 @@ public class Game extends JPanel implements MouseListener, KeyListener, FocusLis
 
 		int rows = 11, colu = 5, y = 40, w = 20, h = 20;
 		for (int i = 0; i <= rows; i++) {
-			
+
 			for (int j = 0; j < colu; j++) {
 				Image[] anim;
 				int wBias = 6;
 				int xBias = 0;
-				if(j == 0) {
+
+				// handle anims on alien
+				if (j == 0) {
 					anim = alien1Frames;
 					wBias = 0;
 					xBias = 2;
-				} else if(j < 3) 
+				} else if (j < 3)
 					anim = alien2Frames;
-				else 
+				else
 					anim = alien3Frames;
-				aliens.add(new Alien(i * 40 + xBias, y - wBias/2, w+wBias, h, anim));
+
+				Alien a = new Alien(i * 40 + xBias, y - wBias / 2, w + wBias, h, anim, j);
+				aliens.add(a);
 				y += h * 2;
+				if (j + 1 == colu) {
+					highestYaliens.add(a);
+				}
 			}
 			y = 40;
 		}
@@ -339,13 +389,13 @@ public class Game extends JPanel implements MouseListener, KeyListener, FocusLis
 		int c = e.getKeyCode();
 		boolean m = true;
 
-		if(c == KeyEvent.VK_SPACE) {
-			if(playState.equals("Playing") && playerLaser == null) {
+		if (c == KeyEvent.VK_SPACE) {
+			if (playState.equals("Playing") && playerLaser == null) {
 				playerLaser = new Laser(player.x);
-			} else if(playState.equals("Paused")) {
+			} else if (playState.equals("Paused")) {
 				playState.equals("Playing");
 			}
-		} else if(c == KeyEvent.VK_P) {
+		} else if (c == KeyEvent.VK_P) {
 			playState = "Paused";
 		}
 
@@ -372,5 +422,15 @@ public class Game extends JPanel implements MouseListener, KeyListener, FocusLis
 			l = m;
 		if (c == KeyEvent.VK_RIGHT)
 			r = m;
+	}
+
+	public static String now() {
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss.SSS");
+		LocalDateTime now = LocalDateTime.now();
+		return " [" + dtf.format(now) + "] ";
+	}
+	
+	public static void println(String message, String reason) {
+		System.out.printf("[%s]" + now() + "%s%n", reason, message);
 	}
 }
